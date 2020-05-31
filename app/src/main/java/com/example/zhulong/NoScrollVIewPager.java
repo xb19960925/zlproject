ecb5aa0eb6975a6b27248daf4aace8805c111e4c
xiong <65000369+xb19960925@users.noreply.github.com>
2020-05-31 15:54:57 +0800

    第二次提交：添加viewpager的禁止滑动、对广告页的布局修改、设置tablayout的item文字点击变色

--- app/src/main/java/com/example/zhulong/NoScrollVIewPager.java
+++ app/src/main/java/com/example/zhulong/NoScrollVIewPager.java
@@ -10,48 +10,49 @@ import androidx.viewpager.widget.ViewPager;
 ;
 
 public class NoScrollVIewPager extends ViewPager {
+    //设置禁止切换viewpager
     private boolean noScroll=false;
-
-    public NoScrollVIewPager(@NonNull Context context, @Nullable AttributeSet attrs) {
-        super(context, attrs);
-    }
 
     public NoScrollVIewPager(@NonNull Context context) {
         super(context);
     }
 
+    public NoScrollVIewPager(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
     public void setNoScroll(boolean noScroll){
         this.noScroll=noScroll;
     }
-
+    //滑动到指定位置
     @Override
     public void scrollTo(int x, int y) {
         super.scrollTo(x, y);
     }
-
+    //触摸事件
     @Override
     public boolean onTouchEvent(MotionEvent ev) {
         if (noScroll){
-            return false;
-        }else {
             return super.onTouchEvent(ev);
+        }else {
+            return false;
         }
     }
-
+    //拦截触摸事件
     @Override
     public boolean onInterceptTouchEvent(MotionEvent ev) {
-        if (noScroll){
-            return false;
-        }else {
+        if (!noScroll){
             return super.onInterceptTouchEvent(ev);
+        }else {
+            return false;
         }
     }
-
+    //设置当前布局，并定义滑动方式
     @Override
     public void setCurrentItem(int item, boolean smoothScroll) {
         super.setCurrentItem(item, smoothScroll);
     }
-
+    //设置当前布局（取消Tablayout点击滑动效果）
     @Override
     public void setCurrentItem(int item) {
         super.setCurrentItem(item,false);
